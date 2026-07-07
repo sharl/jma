c89aa0b213a122b55e526a7305e5ed6332413c32
Sharl Morlaroll <sharl@hauN.org>
2026-07-08 02:10:50 +0900

    データを畳み込んで単一に

--- jma/amedas/data/get_latest_data.py
+++ jma/amedas/data/get_latest_data.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
-import datetime
-
 import jma
 
 
 def get_latest_data(code: str,
                     latest_time: tuple[str, str, str],
-                    ) -> tuple[str, str, dict, dict]:
+                    ) -> tuple[str, dict]:
     yyyymmdd, HH, hh = latest_time
     url = f'https://www.jma.go.jp/bosai/amedas/data/point/{code}/{yyyymmdd}_{hh}.json'
     with jma.session.get(url) as r:
         data = r.json()
         base_key = f'{yyyymmdd}{HH}0000'        # 天気・積雪は1時間毎
         last_key = list(data.keys())[-1]
-        _vars = data.get(base_key)
-        _last = data.get(last_key)
-        return base_key, last_key, _vars, _last
+        _vars = data.get(base_key, {})
+        _last = data.get(last_key, {})
+        # 最新のデータで上書き
+        for k in _last:
+            _vars[k] = _last[k]
+
+        return last_key, _vars
